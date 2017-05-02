546ad9f06e123b3aa75f5eea8c8948505de0e523
Mason Kohls <mason.kohls1999@gmail.com>
2017-05-02 09:57:06 -0700

    powerup class complete. Need to load sounds in settings class and add values to config files

--- entity/powerup.py
+++ entity/powerup.py
@@ -2,17 +2,16 @@ from entity.actor import Entity, Drawable
 from entity.ship import SpaceShip
 from entity.display import Arrow
 from misc import PURPLE
+from assets import texture, sound, value
 
 
 class PowerUp(Entity):
     health = 1
-    behavior = None
-    image = None
-    facing_direction = 0
 
     def __init__(self, instance, position):
         Entity.__init__(self, instance)
         self.position = position
+        self.radius = value["pickup.size"]
         self.arrow = Arrow(self, self.instance, PURPLE, 1)
         self.instance.create(self.arrow)
 
@@ -20,20 +19,23 @@ class PowerUp(Entity):
         if isinstance(kwargs["obj2"], SpaceShip):
             self.pickup_event(kwargs["obj2"])
 
+    def pickup_event(self, ship):
+        pass
+
 
 class HealthPack(PowerUp):
     def create_drawable(self):
-        self.drawable = Drawable(Assets.heart_art, self.position, False, True)
+        self.drawable = Drawable(texture["heart"], self.position, False, True)
 
     def pickup_event(self, ship):
-        Assets.heal_sound.play()
-        ship.damage(-30)
+        sound["heal_sound"].play()
+        ship.damage(value["pickup.heal_pack_amount"])
 
 
 class EarthHealPack(PowerUp):
     def create_drawable(self):
-        self.drawable = Drawable(Assets.repair_earth_art, self.position, False, True)
+        self.drawable = Drawable(texture["repair_earth"], self.position, False, True)
 
     def pickup_event(self, ship):
-        Assets.earth_heal_sound.play()
-        self.instance.earth.damage(-100)
+        sound["earth_heal"].play()
+        self.instance.earth.damage(value["pickup.heal_earth_amount"])
